c26a38348ccc520ab696fe9422b5e9a883a0fe5a
e23001 <e23001@std.it-college.ac.jp>
2023-08-03 20:07:24 +0900

    chapter2 finished

---
+++ pages/blog/index.js
@@ -0,0 +1,14 @@
+import Hero from 'components/hero'
+
+const Blog = () => {
+  return (
+    <>
+      <Hero
+        title='Blog'
+        subtitle='Recent Posts'
+      />
+    </>
+  )
+}
+
+export default Blog
