3c33ce309bfc39b97adee85713df76f100773709
e23001 <e23001@std.it-college.ac.jp>
2023-09-01 20:53:52 +0900

    6.4 finished

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,10 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 
 const Blog = () => (
   <Container>
+    <Meta pageTitle='ブログ' />
     <Hero title='Blog' subtitle='Recent Posts' />
   </Container>
 )
