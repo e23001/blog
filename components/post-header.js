7fa5ebd74ca09e3a0839caca7a8384f28df3856b
Moriyuki Iju <e23001@std.it-college.ac.jp>
2023-11-14 21:56:14 +0900

    chapter9 finished

--- components/post-header.js
+++ components/post-header.js
@@ -6,7 +6,7 @@ import { faClock } from '@fortawesome/free-regular-svg-icons'
 const PostHeader = ({ title, subtitle, publish = '' }) => {
   return (
     <div className={styles.stack}>
-      <p className={styles.subtitle}>{title}</p>
+      <p className={styles.subtitle}>{subtitle}</p>
       <h1 className={styles.title}>{title}</h1>
       {publish && (
         <div className={styles.publish}>
